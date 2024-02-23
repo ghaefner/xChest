a4b3d05d328306066fd913e94b147f65e4a5b851
ghaefner <guillaume-haefner@gmx.de>
2024-02-23 22:31:08 +0100

    Add image generator to module

--- xChest/src/model.py
+++ xChest/src/model.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def split_train_data(dict_folder):
     print("[I] Splitting train data.")
@@ -8,5 +8,49 @@ def split_train_data(dict_folder):
     train_df, dummy_df = train_test_split(dict_folder['train'], train_size=0.8, shuffle= True, random_state= 42)
     valid_df, test_df= train_test_split(dummy_df, train_size= 0.6, shuffle= True, random_state= 42)
 
+    print("[I] Done.")
     return train_df, test_df, valid_df
 
+def generate_images(train_df, test_df, valid_df):
+    print("[I] Generating images fom file list.")
+
+    def scalar(img):
+        return img
+    
+    img_size = (224 ,224)
+    batch_size = 16
+    img_shape= (img_size[0], img_size[1], 3)
+
+    # Define ImageDataGenerator for data augmentation
+    train_datagen = ImageDataGenerator(preprocessing_function=scalar)
+    test_datagen = ImageDataGenerator(preprocessing_function=scalar)
+    valid_datagen = ImageDataGenerator( preprocessing_function=scalar)
+    
+    # Create data generators
+    train_gen = train_datagen.flow_from_dataframe(
+        dataframe=train_df,
+        x_col='filepaths',
+        y_col='label',
+        target_size=img_shape,
+        batch_size=batch_size,
+        class_mode='categorical')
+    
+    test_gen = test_datagen.flow_from_dataframe(
+        dataframe=test_df,
+        x_col='filepaths',
+        y_col='label',
+        target_size=img_shape,
+        batch_size=batch_size,
+        class_mode='categorical')
+    
+    valid_gen = valid_datagen.flow_from_dataframe(
+        dataframe=valid_df,
+        x_col='filepaths',
+        y_col='label',
+        target_size=img_shape,
+        batch_size=batch_size,
+        class_mode='categorical')
+
+    print("[I] Done.")
+   
+    return train_gen, test_gen, valid_gen
