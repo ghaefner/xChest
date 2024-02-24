d85e01b44069bb35c2e932a9b7b5c2d2862ed8ed
ghaefner <guillaume-haefner@gmx.de>
2024-02-24 08:49:42 +0100

    Add hyperparamters to config file

--- xChest/config.py
+++ xChest/config.py
@@ -6,3 +6,50 @@ PATH_SUB = ["train/", "test/", "val/"]
 IMG_SIZE = (244,244)
 IMG_SHAPE = (IMG_SIZE[0], IMG_SIZE[1], 3)
 BATCH_SIZE = 16
+
+# hyper_params = {
+#     'batch_norm_axis': -1,
+#     'batch_norm_momentum': 0.99,
+#     'batch_norm_epsilon': 0.001,
+#     'dense_units': 256,
+#     'kernel_regularizer_l2': 0.016,
+#     'activity_regularizer_l1': 0.006,
+#     'bias_regularizer_l1': 0.006,
+#     'dropout_rate': 0.4,
+#     'dropout_seed': 75,
+#     'learning_rate': 0.0001
+# }
+
+class HyperPars:
+    def __init__(self):
+        self.BATCH_NORM_AXIS = -1
+        self.BATCH_NORM_MOMENTUM = 0.99
+        self.BATCH_NORM_EPSILON = 0.001
+        self.DENSE_UNITS = 256
+        self.KERNEL_REGULARIZER_L2 = 0.016
+        self.ACTIVITY_REGULARIZER_L1 = 0.06
+        self.BIAS_REGULARIZER_L1 = 0.006
+        self.DROPOUT_RATE = 0.04
+        self.DROPOUT_SEED = 75
+        self.LEARNING_RATE = 0.0001
+        self.FUNC_ACTIVATION_RELU = "relu"
+        self.FUNC_ACTIVATION_SOFTMAX = "softmax"
+        self.FUNC_LOSS = "categorical_crossentropy"
+        self.LOSS_METRIC = "accuracy"
+
+    def print_info(self):
+        print("[I] Hyperparameters used:")
+        print("[I] BATCH_NORM_AXIS:", self.BATCH_NORM_AXIS)
+        print("[I] BATCH_NORM_MOMENTUM:", self.BATCH_NORM_MOMENTUM)
+        print("[I] BATCH_NORM_EPSILON:", self.BATCH_NORM_EPSILON)
+        print("[I] DENSE_UNITS:", self.DENSE_UNITS)
+        print("[I] KERNEL_REGULARIZER_L2:", self.KERNEL_REGULARIZER_L2)
+        print("[I] ACTIVITY_REGULARIZER_L1:", self.ACTIVITY_REGULARIZER_L1)
+        print("[I] BIAS_REGULARIZER_L1:", self.BIAS_REGULARIZER_L1)
+        print("[I] DROPOUT_RATE:", self.DROPOUT_RATE)
+        print("[I] DROPOUT_SEED:", self.DROPOUT_SEED)
+        print("[I] LEARNING_RATE:", self.LEARNING_RATE)
+        print("[I] FUNC_ACTIVATION_RELU:", self.FUNC_ACTIVATION_RELU)
+        print("[I] FUNC_ACTIVATION_SOFTMAX:", self.FUNC_ACTIVATION_SOFTMAX)
+        print("[I] FUNC_LOSS:", self.FUNC_LOSS)
+        print("[I] LOSS_METRIC:", self.LOSS_METRIC)
--- xChest/src/model.py
+++ xChest/src/model.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, BatchNormalization
 from tensorflow.keras.optimizers import Adamax
 from tensorflow.keras import regularizers
-from config import BATCH_SIZE, IMG_SHAPE
+from config import BATCH_SIZE, IMG_SHAPE, HyperPars
 
 def split_train_data(dict_folder):
     print("[I] Splitting train data.")
@@ -58,29 +58,38 @@ def generate_images(train_df, test_df, valid_df):
     return train_gen, test_gen, valid_gen
 
 
-def initialize_model(train_gen):
+def initialize_model(train_gen, hyper_params=HyperPars()):
+    print("[I] Initializing Model.")
+    hyper_params.print_info()
+
     gen_dict = train_gen.class_indices
     classes = list(gen_dict.keys())
     num_class = len(classes)
 
     base_model = tf.keras.applications.efficientnet.EfficientNetB3(include_top=False, weights='imagenet', input_shape=IMG_SHAPE, pooling='max')
-    print("Shape of base model output:", base_model.output.shape)
+    print("[I] Base Model Output Shape:", base_model.output_shape)
+
+    batch_norm = BatchNormalization(axis=hyper_params.BATCH_NORM_AXIS, momentum=hyper_params.BATCH_NORM_MOMENTUM, epsilon=hyper_params.BATCH_NORM_EPSILON)
+    dense_layer = Dense(hyper_params.DENSE_UNITS, 
+                        kernel_regularizer=regularizers.l2(hyper_params.KERNEL_REGULARIZER_L2), 
+                        activity_regularizer=regularizers.l1(hyper_params.ACTIVITY_REGULARIZER_L1),
+                        bias_regularizer=regularizers.l1(hyper_params.BIAS_REGULARIZER_L1), 
+                        activation=hyper_params.FUNC_ACTIVATION_RELU)
+    print("[I] Shape after Dense Layer: (None, " + str(hyper_params.DENSE_UNITS) + ")")
+
+    dropout_layer = Dropout(rate=hyper_params.DROPOUT_RATE, seed=hyper_params.DROPOUT_SEED)
+    output_layer = Dense(num_class, activation=hyper_params.FUNC_ACTIVATION_SOFTMAX)
+
     model = Sequential([
         base_model,
-        BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001),
-        Dense(256, kernel_regularizer=regularizers.l2(0.016), activity_regularizer=regularizers.l1(0.006),
-              bias_regularizer=regularizers.l1(0.006), activation='relu'),
-        Dropout(rate=0.4, seed=75),
-        Dense(num_class, activation='softmax')
+        batch_norm,
+        dense_layer,
+        dropout_layer,
+        output_layer
     ])
-    model.compile(Adamax(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
-    
-    '''
-    # Run dummy model for model summary
-    dummy_data = tf.zeros((1, *IMG_SHAPE))
-    dummy_labels = tf.zeros((1, num_class))
-    model.fit(dummy_data, dummy_labels, epochs=1, verbose=0)
 
-    model.summary()
-    '''
+    print("[I] Compiling Model.")
+    model.compile(Adamax(learning_rate=hyper_params.LEARNING_RATE), loss=hyper_params.FUNC_LOSS, metrics=[hyper_params.LOSS_METRIC])
+    print("[I] Done.")
+
     return model
