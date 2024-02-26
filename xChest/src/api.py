64661b7a46b5c921cf9e05d9b67e13c336a482ae
ghaefner <guillaume-haefner@gmx.de>
2024-02-26 21:48:47 +0100

    Add docstrings to api module

--- xChest/src/api.py
+++ xChest/src/api.py
@@ -4,7 +4,16 @@ from pandas import DataFrame, Series, concat
 from config import PATH_BASE, PATH_SUB
 
 def list_files(folder_path) -> DataFrame:
-    filepaths =[]
+    """
+    List files in the specified folder and create a DataFrame with file paths and corresponding labels.
+
+    Args:
+        folder_path (str): Path to the folder containing the files.
+
+    Returns:
+        DataFrame: DataFrame containing file paths and labels.
+    """
+    filepaths = []
     labels = []
     folds = os.listdir(folder_path)
 
@@ -16,14 +25,23 @@ def list_files(folder_path) -> DataFrame:
             filepaths.append(os.path.join(f_path , file))
             labels.append(fold)
             
-    Fseries = Series(filepaths , name = 'filepaths')
-    Lseries = Series(labels , name = 'label')
-    df = concat([Fseries , Lseries] , axis = 1)
+    Fseries = Series(filepaths , name='filepaths')
+    Lseries = Series(labels , name='label')
+    df = concat([Fseries, Lseries], axis=1)
 
     return df
 
 def list_subfolders(path_base=PATH_BASE, path_sub=PATH_SUB):
+    """
+    List files in the subfolders of the specified base folder and create a dictionary of DataFrames.
 
+    Args:
+        path_base (str, optional): Path to the base folder. Defaults to PATH_BASE.
+        path_sub (list of str, optional): List of subfolder paths relative to the base folder. Defaults to PATH_SUB.
+
+    Returns:
+        dict: Dictionary containing subfolder names as keys and corresponding DataFrames as values.
+    """
     dfs = [list_files(path_base+suffix) for suffix in path_sub]
 
     dict_folder = {subfolder.rstrip("/"): df for subfolder, df in zip(path_sub, dfs)}
